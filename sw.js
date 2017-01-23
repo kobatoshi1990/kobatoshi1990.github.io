cdfcf1afd54efe8b8cf8d57ba84c23abc98bdf7a
kobatoshi1990 <ts-kobayashi@kddi-tech.com>
2017-01-23 19:08:20 +0900

    sw modified

--- sw.js
+++ sw.js
@@ -4,18 +4,6 @@
 //   }
 // });
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then(function(registration) {
-    // 登録成功
-    console.log('ServiceWorker registration successful with scope: ', registration.scope);
-  }).catch(function(err) {
-    // 登録失敗 :(
-    console.log('ServiceWorker registration failed: ', err);
-  });
-}
-
-
-
 window.addEventListener('load', function() {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js').then(function(registration) {
