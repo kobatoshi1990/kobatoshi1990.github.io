85029ed86da9250d54977fcd0503391a2940ae46
kobatoshi1990 <ts-kobayashi@kddi-tech.com>
2017-01-23 18:41:14 +0900

    service worker修正

--- sw.js
+++ sw.js
@@ -13,3 +13,27 @@ if ('serviceWorker' in navigator) {
     console.log('ServiceWorker registration failed: ', err);
   });
 }
+
+
+
+window.addEventListener('load', function() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // 登録成功
+        return registration.pushManager.getSubscription().then(function(subscription) {
+          if (subscription) {
+            return subscription
+          }
+          return registration.pushManager.subscribe({
+            userVisibleOnly: true
+          })
+        })
+      }).then(function(subscription) {
+        var endpoint = subscription.endpoint
+        console.log("pushManager endpoint:", endpoint) // https://android.googleapis.com/gcm/send/******:******......
+      }).catch(function(error) {
+        // 登録失敗 :(
+        console.warn("serviceWorker error:", error)
+      })
+  }
+})
