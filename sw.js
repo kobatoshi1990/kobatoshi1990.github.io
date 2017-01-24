eae3359d47b152b57da60cbfd3c7a241172f9dbe
kobatoshi1990 <ts-kobayashi@kddi-tech.com>
2017-01-24 18:45:34 +0900

    s

--- sw.js
+++ sw.js
@@ -1,21 +1,21 @@
-window.addEventListener('load', function() {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // 登録成功
-        // return registration.pushManager.getSubscription().then(function(subscription) {
-        //   if (subscription) {
-        //     return subscription;
-        //   }
-        //   return registration.pushManager.subscribe({
-        //     userVisibleOnly: true
-        //   })
-        // })
-      //}).then(function(subscription) {
-      //  var endpoint = subscription.endpoint;
-      //  console.log(endpoint); // https://android.googleapis.com/gcm/send/******:******......
-      }).catch(function(error) {
-        // 登録失敗 :(
-        console.warn("serviceWorker error:", error);
-      })
-  }
-})
+// window.addEventListener('load', function() {
+//   if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('/sw.js').then(function(registration) {
+//       // 登録成功
+//         return registration.pushManager.getSubscription().then(function(subscription) {
+//           if (subscription) {
+//             return subscription;
+//           }
+//           return registration.pushManager.subscribe({
+//             userVisibleOnly: true;
+//           })
+//         })
+//       }).then(function(subscription) {
+//         var endpoint = subscription.endpoint;
+//         console.log(endpoint); // https://android.googleapis.com/gcm/send/******:******......
+//       }).catch(function(error) {
+//         // 登録失敗 :(
+//         console.warn("serviceWorker error:", error);
+//       })
+//   }
+// })
